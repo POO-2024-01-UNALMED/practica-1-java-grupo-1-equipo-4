a8acf776d1edd936f46e6357e5c44b694c4641c2
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-03-21 18:11:12 -0500

    Clase Persona

--- gestorAplicación/procesoAdopcion/persona.java
+++ gestorAplicación/procesoAdopcion/persona.java
@@ -1,5 +1,68 @@
 package gestorAplicación.procesoAdopcion;
 
-public class persona {
-    
+public class Persona {
+	
+	private String nombre;
+	private int edad;
+	private long cedula;
+	private String direccion;
+	private long telefono;
+
+	
+	//CONSTRUCTOR
+	public Persona(String nombre,int edad, long cedula, String direccion, long telefono) {
+		this.nombre= nombre;
+		this.edad= edad;
+		this.cedula= cedula;
+		this.telefono= telefono;
+		this.direccion= direccion;
+	}
+	
+	//MÉTODOS SETTER Y GETTER
+	
+	public void setNombre(String nombre) {
+		this.nombre= nombre;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad= edad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setCedula(long cedula) {
+		this.cedula= cedula;
+	}
+	public long getCedula() {
+		return cedula;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion= direccion;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setTelefono(long telefono) {
+		this.telefono= telefono;
+	}
+	
+	public long getTelefono() {
+		return telefono;
+	}
+	
+	//MÉTODO TO STRING
+	
+	public String toString() {
+		return "Nombre: " + getNombre() + "\nEdad: "+ getEdad()+ "\nCedula: " + getCedula() +"\nTelefono: " 
+				+ getTelefono() + "\nDireccion "+ getDireccion() + "\n";
+	}
+
 }
+
+
