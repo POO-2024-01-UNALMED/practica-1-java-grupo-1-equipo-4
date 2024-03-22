2e346fc92d5d9fbed37f2ce8ab4e3aedcb357797
danzapata <142749695+danzapata@users.noreply.github.com>
2024-03-21 23:16:53 -0500

    Update tienda.java

--- gestorAplicación/servicioAdicional/tienda.java
+++ gestorAplicación/servicioAdicional/tienda.java
@@ -1,5 +1,52 @@
 package gestorAplicación.servicioAdicional;
+import gestorAplicación.procesoAdopcion.Persona;
+import java.util.ArrayList;
 
 public class Tienda {
-    
+        //Atributos 
+        private ArrayList<Producto> productos = new ArrayList<> ();
+        private ArrayList<Empleado> empleados = new ArrayList<> ();
+        private ArrayList<Persona> voluntario = new ArrayList<> ();
+        
+        //constructor 
+        public Tienda(Empleado empleado){
+            this.empleados.add(empleado); //Tienda con un empleado 
+        }
+
+        public Tienda(){
+            //tienda sin empleados
+        }
+
+
+        //métodos 
+        public void agregarEmpleado(Empleado empleado){
+            this.empleados.add(empleado);
+        }
+        public void empleadosTienda(){
+            for (int i = 0; i<empleados.size(); i++){
+                System.out.println(empleados.get(i));
+            }
+        }
+
+        public void agregarProducto(Producto producto){ //se agrega un producto si hay un empleado que lo agregue
+            if(empleados!=null){
+                productos.add(producto);
+            }
+            else{
+                System.out.println("No hay empleado para agregar producto");
+            }
+        }
+
+        public void inventario(){ //inventario se hace si hay un empleado
+            if(empleados!=null){
+                for (int i = 0; i<productos.size(); i++){
+                    System.out.println(productos.get(i));
+                }
+            }
+            else{
+                System.out.println("No hay empleado para hacer inventario");
+            }
+        }
+
+
 }
