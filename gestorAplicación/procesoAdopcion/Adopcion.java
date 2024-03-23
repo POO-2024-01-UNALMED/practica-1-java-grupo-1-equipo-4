c3ccc0893997e8c8dc87d556a22b5fb19a851c63
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-03-23 17:59:00 -0500

    clase Adopcion
    se escribieron sus atributos, toString y métodos setter y getter.

--- gestorAplicación/procesoAdopcion/Adopcion.java
+++ gestorAplicación/procesoAdopcion/Adopcion.java
@@ -1,5 +1,47 @@
 package gestorAplicación.procesoAdopcion;
 
+import java.time.LocalDate;
+
 public class Adopcion {
     
+	private Animal animal;
+	private Persona persona;
+	private LocalDate fechaAdopcion;
+	
+	//constructor
+	
+	public Adopcion(Animal animal, Persona persona) {
+		this.animal = animal;
+		this.persona = persona;
+	}
+	
+	//setter y getter
+	
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+	
+	public Animal getAnimal() {
+		return animal;
+	}
+	
+	public void setAdoptante(Persona persona) {
+		this.persona = persona;
+	}
+	
+	public Persona getAdoptante() {
+		return persona;
+	}
+	
+	public LocalDate getFechaAdopcion() {
+		return fechaAdopcion;
+	}
+	
+	//toString
+	
+	public String toString() {
+		return "Mascota: " + getAnimal() + "\nAdoptante: " + getAdoptante() + "\nFecha de adopcion: " + getFechaAdopcion();
+	}
+	
+	//FALTA ENCUESTA
 }
