e2361897ed8599e270f83a4d15e3ce52dd04aa61
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-22 21:02:41 -0500

    0

--- gestorAplicación/servicioAdicional/empleado.java
+++ gestorAplicación/servicioAdicional/empleado.java
@@ -1,17 +1,29 @@
 package gestorAplicación.servicioAdicional;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Empleado 
 {
+    private static ArrayList <Empleado> empleado;
     private String nombre;
     private long cedula;
     private Rol profesion;
     private ArrayList <Cita> horario;
+    //CONTADOR NUMERO DE EMPLEADOS
+    private int cEmpleados = 0;
 
     //CONSTRUCTOR
     public Empleado(String nombre, long cedula, Rol profesion, ArrayList<Cita> horario)
     {
+        //Scaner SI o NO crear empleado
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("¿Desea agregar un nuevo empleado? ");
+        String r = scanner.nextLine();
+        scanner.close();
+
+        System.out.println(r);
+
     }
 
     //toString
