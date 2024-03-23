e727d59e9381fc72f40a1844ec6203ef7e0a5385
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-23 01:46:05 -0500

    777

--- gestorAplicación/Main.java
+++ gestorAplicación/Main.java
@@ -2,14 +2,29 @@ package gestorAplicación;
 
 import gestorAplicación.servicioAdicional.*;
 import gestorAplicación.procesoAdopcion.*;
+import java.util.Scanner;
 
 public class Main 
 {
     public static void main(String[] args){
 
         //PRUEBA DE CODIGO EMPLEADO
-
         Empleado empleado1 = new Empleado("Juan", 10, "Veterinario");
         System.out.println(empleado1.getNombre() + empleado1.getCedula() + empleado1.getprofesion());
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.next();
+
+        System.out.println("Ingrese su cedula: ");
+        int cedula = scanner.nextInt();
+
+        System.out.println("Ingrese su profesión: ");
+        String profesion = scanner.next();
+        scanner.close();
+
+        Empleado empleado2 = new Empleado(nombre, cedula, profesion);
+        System.out.println(empleado2.getNombre() + " " + empleado2.getCedula() + " " + empleado2.getprofesion());
+
     }  
 }
