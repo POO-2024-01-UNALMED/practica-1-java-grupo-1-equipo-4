6f5fc70da9fe7e6fe1ba83cffc1f82e720788eac
Nsja2806 <nizambranom@unal.edu.co>
2024-03-25 15:33:35 -0500

    Creación cita
    Se crea la clase cita

--- gestorAplicación/servicioAdicional/cita.java
+++ gestorAplicación/servicioAdicional/cita.java
@@ -1,5 +1,20 @@
 package gestorAplicación.servicioAdicional;
 
 public class cita {
-    
+    private String hora;
+    private String fecha;
+    private boolean disponibilidad;
+
+// Set&Get disponibilidad
+public void setDisponibilidad(boolean disponibilidad) {
+    this.disponibilidad = disponibilidad;
+}
+public boolean getDisponibilidad() {
+    return disponibilidad;
+}
+
+@Override
+public String toString() {
+    return "cita [hora=" + hora + ", fecha=" + fecha + ", disponibilidad=" + disponibilidad + "]";
+}
 }
--- gestorAplicación/servicioAdicional/finanza.java
+++ gestorAplicación/servicioAdicional/finanza.java
@@ -1,1 +1,60 @@
 package gestorAplicación.servicioAdicional;
+public class finanza{
+    private enum clasificacion{
+        NOMINA, SERVICIO, IMPUESTO
+    };
+    private long salidas;
+    private long ingresos;
+    private long patrimonio=1000000;
+    private long donacion;
+    private clasificacion Clasificacion;
+
+public void pagoAdministracion (clasificacion Clasificacion, int cantidad){
+    if (Clasificacion==clasificacion.NOMINA) {
+
+    }
+}
+public String ventaNueva (Producto producto, int cantidad){
+
+}
+// Getter & Setter
+public long getSalidas() {
+    return salidas;
+}
+
+public void setSalidas(long salidas) {
+    this.salidas = salidas;
+}
+
+public long getIngresos() {
+    return ingresos;
+}
+
+public void setIngresos(long ingresos) {
+    this.ingresos = ingresos;
+}
+
+public long getPatrimonio() {
+    return patrimonio;
+}
+
+public void setPatrimonio(long patrimonio) {
+    this.patrimonio = patrimonio;
+}
+
+public long getDonacion() {
+    return donacion;
+}
+
+public void setDonacion(long donacion) {
+    this.donacion = donacion;
+}
+
+public clasificacion getClasificacion() {
+    return Clasificacion;
+}
+
+public void setClasificacion(clasificacion clasificacion) {
+    Clasificacion = clasificacion;
+}
+}
