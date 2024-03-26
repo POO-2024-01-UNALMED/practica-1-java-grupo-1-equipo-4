5755eba8e39fc95d3d785ce2f0cb6bccabeedc5d
xXSirAloneXx <jsalazargo@unal.edu.co>
2024-03-25 20:49:29 -0500

    a

--- gestorAplicación/servicioAdicional/Empleado.java
+++ gestorAplicación/servicioAdicional/Empleado.java
@@ -30,7 +30,7 @@ public class Empleado extends Persona {
             return rol;
         }
     }
-    //FALTA ARRAYLIST HORARIOS Y ARREGLAR EL ENUM ROL DE PROFESION(CONVIENE ENUM O SWITCH?)
+    //FALTA ARRAYLIST HORARIOS 
 
     //CONSTRUCTOR
     public Empleado(String nombre,int edad, long cedula, String direccion, long telefono, Rol profesion, ArrayList <Cita> horario)
