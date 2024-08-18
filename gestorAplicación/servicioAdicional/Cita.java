303d662739cf26e1609b38caf79ae281e54afe43
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-08-18 17:17:04 -0500

    Funcionalidad Agendar_Servicios

--- gestorAplicación/servicioAdicional/Cita.java
+++ gestorAplicación/servicioAdicional/Cita.java
@@ -8,6 +8,10 @@ public class Cita{
     private Animal animal;
     private LocalDate fecha;
     private String estado; 
+    private Empleado empleado;
+    private Cupo cupo;
+    private double costo;
+    
     
     public Cita(Cliente cliente, Animal animal, LocalDate fecha) {
     	this.cliente=cliente;
@@ -15,6 +19,34 @@ public class Cita{
     	this.fecha=fecha;
     	this.estado="pendiente";
     	}
+    
+    public Cita(Cliente cliente, Animal animal,Empleado empleado, Cupo cupo, int servicio ) {
+    	
+    	this.cliente=cliente;
+    	this.animal=animal;
+    	this.empleado=empleado;
+    	
+    	cupo.setDisponible(false);//PARA MARCAR QUE EL CUPO YA NO ESTARÁ DISPONIBLE 	
+    	this.cupo=cupo;
+    	
+    	if (servicio==1) {
+    		this.costo= 10000;//COSTO DE UNA CITA DE GUARDERÍA
+    		this.cliente.agregar_puntos(3); //POR CITA DE GUARDERIA GANA 3 PUNTOS
+    	}
+    	
+        if (servicio==2) {
+        	this.costo=20000; //COSTO DE UNA CITA DE VETERINARIA
+        	this.cliente.agregar_puntos(5);//POR CITA DE VETERINARIA GANA 5 PUNTOS
+    		}
+        
+        if (servicio==3) {
+        	this.costo=15000; //COSTO DE UNA CITA DE PELUQUERÍA
+        	this.cliente.agregar_puntos(4);//POR CITA DE PELUQUERIA GANA 4 PUNTOS
+        }
+    		
+    	
+    	
+    }
     
 	public Cliente getCliente() {
         return cliente;
--- gestorAplicación/servicioAdicional/Empleado.java
+++ gestorAplicación/servicioAdicional/Empleado.java
@@ -1,83 +1,242 @@
 package gestorAplicación.servicioAdicional;
 
 import  gestorAplicación.procesoAdopcion.Persona;
+
+
 import gestorAplicación.servicioAdicional.Empleado.Rol;
 import gestorAplicación.servicioAdicional.Cita;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+
 public class Empleado extends Persona {
 	
-    private Rol profesion;
-  //  private Map<String, List<String>>[] horario = new Map[15];//
-    private Map<String, List<Cita>>horario;
+	
+	//ATRIBUTOS
+    private Rol profesion; 
     
     public static enum Rol {
         VETERINARIO,PELUQUERO,CUIDADOR,TENDERO;
-
     }
-      ArrayList<String> [] horarioo;
+    
+    ArrayList<ArrayList<Cupo>> agenda_dias = new ArrayList<>();
+    
+    //CONSTRUCTOR
+    
+    public Empleado( String nombre,int edad, long cedula,long telefono, String direccion,Rol profesion) {
+    	super (nombre,edad,cedula,telefono,direccion);
+        this.profesion = profesion;
+        
+        if (profesion!= Rol.TENDERO) { //EL TENDERO NO OFRCE CITAS.
+        	this.llenar_agenda();
+        }
+        	
+        
+    }
+    
+    
+    //MÉTODOS
+    
+    private void llenar_agenda() {
+    	
+    	//CANTIDAD DE CICLOS, DEPENDIENDO EL DIA.
+    	int [] ciclos = {6,5,4,3,2,1};
+    	
+    	LocalDate fechaActual = LocalDate.now();
+    	
+    	int dia_semana = fechaActual.getDayOfWeek().getValue();//NUMERO DEL DIA DE LA SEMANA.
+    	
+    	//LLENAR LOS DÍAS RESTANTES DE LA SEMANA:
+    	
+        //CUANDO ES DOMINGO, SE HABILITAN LOS CUPOS DEL LUNES SIGUIENTE HASTA EL SÁBADO.
+    	if (dia_semana==7) {
+    		
+    		fechaActual = fechaActual.plusDays(1); //PARA OMITIR EL DOMINGO
+    		
+    		int j=0; 		
+    		while(j < 6) {
+    			
+    			ArrayList<Cupo> cupos_dia= new ArrayList<>();
+    			
+	    		cupos_dia.add(new Cupo(fechaActual, "8:00","10:00", true));
+	    		cupos_dia.add(new Cupo(fechaActual, "10:00","12:00", true));
+	    		cupos_dia.add(new Cupo(fechaActual, "14:00","16:00", true));
+	    		cupos_dia.add(new Cupo(fechaActual, "16:00","18:00", true));
+	    		
+	    		j++;
+	    		
+	    		this.agenda_dias.add(cupos_dia);
+	    		fechaActual = fechaActual.plusDays(1);//PASAR AL SIGUIENTE DÍA.
+    		}	
+    	}
+    	else {
+    		
+    	//CUANDO ES UN DÍA DEFERENTE AL DOMINGO, SE HABILITAN LOS CUPOS DE LOS DIAS RESTANTES DE LA SEMANA:
+    	int i=0;
+    	while ( i < ciclos[dia_semana-1]) {
+    		
+    		ArrayList<Cupo> cupos_dia= new ArrayList<>();
+	
+    		cupos_dia.add(new Cupo(fechaActual, "8:00","10:00", true));
+    		cupos_dia.add(new Cupo(fechaActual, "10:00","12:00", true));
+    		cupos_dia.add(new Cupo(fechaActual, "14:00","16:00", true));
+    		cupos_dia.add(new Cupo(fechaActual, "16:00","18:00", true));
+    		
+    		i++;
+    		
+    		this.agenda_dias.add(cupos_dia);
+    		
+    		fechaActual = fechaActual.plusDays(1);//PASAR AL SIGUIENTE DÍA.
+    		//DEJA LA FECHA HASTA EL DOMINGO
+    	}
+    	
+    	//-----------------------------------------------
+    	
+    	//LLENAR LOS DIAS QUE FALTARON, ES DECIR LOS ANTERIORES, PERO PARA LA SIGUIENTE SEMANA LA OTRA SEMANA
+    	
+    	if (dia_semana != 1 && dia_semana!=7) { //COMO EL DOMINGO Y EL LUNES HABILITAN TODOS LOS DÍAS DE LA SEMANA
+    		                                    //ENTONCES NO ES NECESARIO QUE ESTÉN AQUÍ.
+    		
+    		fechaActual = fechaActual.plusDays(1); //OMITIR EL DOMINGO
+    		
+    		int j=0;  		
+    		while(j < (dia_semana-1)){
+    			
+	    		ArrayList<Cupo> cupos_dia= new ArrayList<>();
+    			
+	    		cupos_dia.add(new Cupo(fechaActual, "8:00","10:00", true));
+	    		cupos_dia.add(new Cupo(fechaActual, "10:00","12:00", true));
+	    		cupos_dia.add(new Cupo(fechaActual, "14:00","16:00", true));
+	    		cupos_dia.add(new Cupo(fechaActual, "16:00","18:00", true));
+	    		
+	    		j++;
+	    		
+	    		this.agenda_dias.add(cupos_dia);	
+	    		fechaActual = fechaActual.plusDays(1);//PASAR AL SIGUIENTE DÍA.			
+	    		
+    		}
+    	}
+    }
+}
+    
+    
+    public void actualizar_Agenda() {
+    	
+    	//ACTUALIZAMOS HORARIO
+    		
+    	//RECORREMOS EL ARRAY LIST, QUE CONTIENE ARRAYLIST DE CUPOS POR DIA.
+    	for(ArrayList <Cupo> Array_dia : this.agenda_dias) {
+    		
+    		LocalDate fecha_Actual = LocalDate.now(); //LA FECHA DE ACTUALIZACIÓN
+    		
+    		//COMPROBAR SI LA FECHA DE LOS CUPOS ES ANTERIOR A LA ACTUAL
+    		if (Array_dia.get(0).getDia().isBefore(fecha_Actual)) {
+    			
+    			//SI LA FECHA EFECTIVAMENTE ES ANTERIOR A LA ACTUAL:
+    			
+    			//SI EL DIA ("LUNES", "MARTES"...) COINCIDE CON LA ACTUAL, ENTONCES SE HABILITAN LOS CUPOS PARA HOY.
+    			if (fecha_Actual.getDayOfWeek().getValue() == Array_dia.get(0).getDia().getDayOfWeek().getValue()) {
+    				
+					Array_dia.clear();//LIMPIAR EL ARRAYLIST DE ESE DÍA.
+					
+					//LLENARLO CON NUEVOS CUPOS.
+		    		Array_dia.add(new Cupo(fecha_Actual, "8:00","10:00", true));
+		    		Array_dia.add(new Cupo(fecha_Actual, "10:00","12:00", true));
+		    		Array_dia.add(new Cupo(fecha_Actual, "14:00","16:00", true));
+		    		Array_dia.add(new Cupo(fecha_Actual, "16:00","18:00", true));	
+		    		
+    			}
+    			
+    			else {
+    				
+    				 fecha_Actual = LocalDate.now();
+    							 
+    				 int num_dia=Array_dia.get(0).getDia().getDayOfWeek().getValue();
+    				
+    				 boolean continuar = true;	
+    				
+  
+    			    while(continuar) {
+    			    	
+    			    	//SI EL DIA ("LUNES", "MARTES"..) NO COINCIDE CON EL ACTUAL, ENTONCES BUSCAMOS CON EL QUE 
+    				   //COINCIDA EN LOS DIAS PRÓXIMOS.
+    			    		
+    				      fecha_Actual = fecha_Actual.plusDays(1); //AUMENTAMOS AL DIA SIGUIENTE
+    				       				
+     				     if (fecha_Actual.getDayOfWeek().getValue() == num_dia)  {	    				    	
+    				    	
+    				       Array_dia.clear();//LIMPIAR EL ARRAYLIST DE ESE DÍA.
+    					
+    					   //LENARLO EL DÍA POR DELANTE
+    		    		   Array_dia.add(new Cupo(fecha_Actual, "8:00","10:00", true));
+    		    		   Array_dia.add(new Cupo(fecha_Actual, "10:00","12:00", true));
+    		    		   Array_dia.add(new Cupo(fecha_Actual, "14:00","16:00", true));
+    		    		   Array_dia.add(new Cupo(fecha_Actual, "16:00","18:00", true));
+    		    		
+    		    		   continuar = false;
+    			        }
+     				}
+    			}
+    		}
+    	}
+    }
+    
+    
+    ///COMPROBAR SI TIENE CUPOS DISPONIBLES
+    public boolean tieneCupos() {
+
+    	Boolean booleano = false;
+    	this.actualizar_Agenda();
+    	
+    	for(ArrayList <Cupo> cupos_por_dia: this.agenda_dias) {
+    		
+    		for(Cupo cupo: cupos_por_dia) {
+    			
+    			if (cupo.isDisponible()==true) {
+    				booleano=true;
+    				break;
+    			}
+    		}
+    	}
+    	return booleano;
+    }
+    
+    public ArrayList<Cupo> cupos_disponibles(int i){
+    	
+    	ArrayList<Cupo> cupos_disponibles= new ArrayList<>();
+    	
+    	for(ArrayList <Cupo> Array_dia : this.agenda_dias) {
+    		
+    		if (Array_dia.get(0).getDia().getDayOfWeek().getValue()==i) {
+    			
+    			for(Cupo cupo: Array_dia) {
+    				
+    				if(cupo.isDisponible()) {
+    				cupos_disponibles.add(cupo);
+    				}
+    			}	
+    			break;
+    		}
+    	}
+    	
+    	return cupos_disponibles;
+    }
+    
+    
+    
+    
+    
+    
+ 
 
         public Rol getProfesion(){
             return profesion;
         }
-    //FALTA ARRAYLIST HORARIOS 
 
-    /*CONSTRUCTOR
-        public Empleado( String nombre,int edad, long cedula,long telefono, String direccion,Rol profesion, Map<String, List<String>>[] horario)
-        {
-         super (nombre,edad,cedula,telefono,direccion);
-            this.profesion = profesion;
-            this.horario = horario;
-        }*/
-        
-        public Empleado( String nombre,int edad, long cedula,long telefono, String direccion,Rol profesion) {
-        	super (nombre,edad,cedula,telefono,direccion);
-            this.profesion = profesion;
-            this.horario = new HashMap<>();
-            llenarHorario();
-        }
            
-        private void llenarHorario() {
-            String[] diasSemana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
-            
-            if (this.profesion == Empleado.Rol.CUIDADOR) {
-            	for (String dia : diasSemana) {
-                horario.put(dia, new ArrayList<>());
-                horario.get(dia).add(new Cita("8:00", "10:00"));
-                horario.get(dia).add(new Cita("10:00", "12:00"));
-                horario.get(dia).add(new Cita("14:00", "16:00"));
-                horario.get(dia).add(new Cita("16:00", "18:00"));
-                }
-            }
-            else {
-            	for (String dia : diasSemana) {
-                    horario.put(dia, new ArrayList<>());
-                    horario.get(dia).add(new Cita("8:00", "9:00"));
-                    horario.get(dia).add(new Cita("9:00", "10:00"));
-                    horario.get(dia).add(new Cita("10:00", "11:00"));
-                    horario.get(dia).add(new Cita("11:00", "12:00"));
-                    horario.get(dia).add(new Cita("14:00", "15:00"));
-                    horario.get(dia).add(new Cita("15:00", "16:00"));
-                    horario.get(dia).add(new Cita("16:00", "17:00"));
-            	}
-            }
-        }
-        
-        public List<Cita> obtenerCitas(String dia) {
-            return horario.getOrDefault(dia, new ArrayList<>());
-        }
-        
-        public boolean cuposDisponibles(String dia) {
-        	 for (Cita cita : this.obtenerCitas(dia)) {
-	            	if (cita.getDisponibilidad()==true) {
-	            		return true;
-	            	}
-              }
-        	 return false;
-       }
+
+      
         
         
 
