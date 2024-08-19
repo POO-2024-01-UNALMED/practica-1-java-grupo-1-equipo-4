3e3feeebd497464bcded8b0b471e94372a06fa40
danzapata <142749695+danzapata@users.noreply.github.com>
2024-08-19 16:26:16 -0500

    tienda

--- gestorAplicación/servicioAdicional/Tienda.java
+++ gestorAplicación/servicioAdicional/Tienda.java
@@ -1,5 +1,6 @@
 package gestorAplicación.servicioAdicional;
 import gestorAplicación.procesoAdopcion.CentroAdopcion;
+import gestorAplicación.procesoAdopcion.Cliente;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -215,6 +216,49 @@ public class Tienda implements Serializable{
                 return "No hay un empleado para atenderlo.";
             }
         }
+        
+        //TODO: MÉTODOS DE PRUEBA-------------------------------------------------------------------------------------------- 
+        
+        public String compra(int indice, int unidades, Cliente cliente){
+            if (empleados!=null){
+                indice -= 1; //CONVERTIMOS EL NÚMERO A INDICE
+                
+                if (indice>=productos.size() || indice<0) { //COMPROBAMOS SI EL INDICE ES CORRECTO 
+                	return "No pudimos realizar la compra, el índice es incorrecto.";
+                }
+                int cantidad = productos.get(indice).getCantidadUnidades();
+
+                if (cantidad!=0 && cantidad>=unidades){//SI LA CANTIDAD INGRESADA ESTÁ DENTRO DEL RANGO
+                	
+                    cantidad-=unidades;
+                    productos.get(indice).setCantidadUnidades(cantidad);
+                    String nombre = productos.get(indice).getNombre();
+                    String tipo = productos.get(indice).getTipoAnimal(); 
+                    if (productos.get(indice).getCantidadUnidades()==0) {
+                    	productos.remove(indice);
+                    	
+                    }
+                    
+                    cliente = CentroAdopcion.isCliente(cliente); // SE COMPRUEBA SI EL CLIENTE EXISTE 
+                    float precio = productos.get(indice).getPrecio();
+                    int puntos = cliente.getPuntos(); //SE TOMAN LOS PUNTOS 
+                    
+                    if (puntos>=15) {
+                    	cliente.disminuir_puntos(puntos);
+                    	 return "-------------------------------------------\n"+"Has comprado "+unidades+" unidades de: "+nombre+" para "+tipo+"\n"+"Total a pagar: "+precio*unidades*0.1+" $"+"\n"+"Han sido utilizados 15 puntos para un 10% de descuento\n"+"Puntos restantes: "+cliente.getPuntos()+"\n-------------------------------------------";
+                    }else {
+                    	return "-------------------------------------------\n"+"Has comprado "+unidades+" unidades de: "+nombre+" para "+tipo+"\n"+"Total a pagar: "+precio*unidades+" $"+"\n"+"No ha sido posible usar puntos para descuento\n"+"-------------------------------------------";
+                    }
+                }
+                else{
+                    return "No se pudo realizar la compra, no hay unidades suficientes del producto solicitado.";
+                }
+            }else{
+                return "No hay un empleado para atender en estos momentos.";
+            }
+        }
+        
+        //-----------------------------------------------------------------------------------------------------------------------------
         
         public static boolean isCliente(long cedula) {
         	boolean respuesta = false;
