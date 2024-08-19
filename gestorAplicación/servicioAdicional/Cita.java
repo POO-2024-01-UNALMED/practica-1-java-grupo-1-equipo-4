4902e3b2abe2a774f5931c5cad953de664c2cbb9
Nsja2806 <nizambranom@unal.edu.co>
2024-08-18 20:25:59 -0500

    Cita orden

--- gestorAplicación/servicioAdicional/Cita.java
+++ gestorAplicación/servicioAdicional/Cita.java
@@ -12,7 +12,13 @@ public class Cita{
     private Cliente cliente2;
     private Animal animal2;
     private LocalDate fecha;
-    private EstadoCita estado;
+    private EstadoCita Estado;
+    private String estado;
+    private Cupo cupo;
+    private Empleado empleado;
+    private Cliente cliente;
+    private double costo;
+    
     
     public Cita(Cliente cliente1, Cliente cliente2, Animal animal, Animal animal2, LocalDate fecha) {
     	this.cliente1=cliente1;
@@ -20,9 +26,35 @@ public class Cita{
     	this.animal=animal;
     	this.animal2=animal2;
     	this.fecha=fecha;
-    	this.estado=EstadoCita.PENDIENTE;
     	}
-    //empleado,cupo,costo//
+
+    public Cita(Cliente cliente, Animal animal,Empleado empleado, Cupo cupo, int servicio ) {
+    	
+    	this.cliente=cliente;
+    	this.animal=animal;
+    	this.empleado=empleado;
+    	
+    	cupo.setDisponible(false);//PARA MARCAR QUE EL CUPO YA NO ESTARÁ DISPONIBLE 	
+    	this.cupo=cupo;
+    	
+    	if (servicio==1) {
+    		this.costo= 10000;//COSTO DE UNA CITA DE GUARDERÍA
+    		this.cliente.agregar_puntos(3); //POR CITA DE GUARDERIA GANA 3 PUNTOS
+    	}
+    	
+        if (servicio==2) {
+        	this.costo=20000; //COSTO DE UNA CITA DE VETERINARIA
+        	this.cliente.agregar_puntos(5);//POR CITA DE VETERINARIA GANA 5 PUNTOS
+    		}
+        
+        if (servicio==3) {
+        	this.costo=15000; //COSTO DE UNA CITA DE PELUQUERÍA
+        	this.cliente.agregar_puntos(4);//POR CITA DE PELUQUERIA GANA 4 PUNTOS
+        }
+    		
+    	
+    	
+    }
     
 	public Cliente getCliente() {
         return cliente1;
@@ -45,11 +77,11 @@ public class Cita{
     }
 
     public EstadoCita getEstado() {
-        return estado;
+        return Estado;
     }
 
     public void setEstado(EstadoCita estado) {
-        this.estado = estado;
+        this.Estado = estado;
     }
     
     public void setFecha(LocalDate fecha) {
