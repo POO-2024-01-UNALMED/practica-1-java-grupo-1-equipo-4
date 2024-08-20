2745874bddfbedd4d0c03faad63dc6b5bc25ad7e
danzapata <142749695+danzapata@users.noreply.github.com>
2024-08-20 08:52:23 -0500

    Tienda comentado

--- gestorAplicacion/administracion/Tienda.java
+++ gestorAplicacion/administracion/Tienda.java
@@ -105,25 +105,26 @@ public class Tienda implements Serializable{
         public void agregarEmpleado(Empleado empleado){
             Tienda.empleados.add(empleado);
         }
-        public ArrayList<Empleado> empleadosTienda(){ //muestra todos los empleados de la tienda 
+        public ArrayList<Empleado> empleadosTienda(){ //DEVUELVE EL ARRAY DE EMPLEADOS  
             return empleados;
         }
 
         public void agregarProducto(Producto producto){ 
-            if(empleados!=null){     //se agrega un producto si hay un empleado que lo agregue
+            if(empleados!=null){     // SI HAY UN EMPLEADO, SE AGREGA EL PRODUCTO DEL PARÁMETRO
                 productos.add(producto);
             }
         }
 
-        public String inventario(){ //inventario se hace si hay un empleado
-            if(empleados!=null){
-            	String resultado = "";
-            	int indice = 1;
+        public String inventario(){ //INVENTARIO 
+            if(empleados!=null){// SI HAY UN EMPLEADO SE HACE INVENTARIO 
+            	String resultado = ""; //STRING DONDE SE VA A GUARDAR EL TEXTO 
+            	int indice = 1; // INDICE QUE SE VA A MOSTRAR ENCIMA DEL PRODUCTO 
+            	
             	for (int i = 0; i<productos.size();i++) {
-            		resultado += indice+"."+productos.get(i).toString();
+            		resultado += indice+"."+productos.get(i).toString(); // RECORREMOS LA LISTA, CONCATENANDO EL INDICE CON EL PROTUCTO EN STRING
             		indice = indice+1;
             	}
-                return resultado;
+                return resultado; // SE DEVULVE EL RESULTADO
             }
             else {
                 return "No hay empleado para atender la tienda";
@@ -131,13 +132,13 @@ public class Tienda implements Serializable{
         }
         
         public String filtrar(String tipo){ //FILTRAR
-            if(empleados!=null){
-            	String resultado = "";
+            if(empleados!=null){ // SI HAY POR LO MENOS UN EMPLEADO EN LA TIENDA, SE HACE EL FILTRADO DE PRODUCTOS 
+            	String resultado = ""; //SE CREA UN STRING DONDE SE VA A ALMACENAR LOS PRODUCTOS CON UN ÍNDICE
             	int indice = 0;
-            	for (int i = 0; i<productos.size();i++) {
-              		indice = indice+1;
-            		if (productos.get(i).getTipoAnimal().equals(tipo) || productos.get(i).getTipoAnimal().equals("Uso general")) {
-            			resultado += indice+"."+productos.get(i).toString();
+            	for (int i = 0; i<productos.size();i++) { // SE RECORRE LA LISTA 
+              		indice = indice+1; //ÍNDICE QUE SE MUESTRA POR UNCIMA DEL PRODUCTO 
+            		if (productos.get(i).getTipoAnimal().equals(tipo) || productos.get(i).getTipoAnimal().equals("Uso general")) { // SI LOS PRODUCTOS SON DE USO GENERAL, O DEL TIPO DEL ANIMAL INGRESADO
+            			resultado += indice+"."+productos.get(i).toString(); // SE ANEXAN LOS PRODUCTOS AL STRING QUE SE VA A RETORNAR
             		}
             	}
                 return resultado;
@@ -147,33 +148,37 @@ public class Tienda implements Serializable{
             }
         }
 
-        //TODO: MÉTODOS DE PRUEBA-------------------------------------------------------------------------------------------- 
+        // MÉTODOS DE COMPRA-------------------------------------------------------------------------------------------- 
         public String compra(int indice, Cliente cliente){
-            if (empleados!=null){
-                indice -= 1;//CONVERTIMOS EL NÚMERO A INDICE
+        	
+            if (empleados!=null){ // SE COMPRUEBA SI HAY UN EMPLEADO PARA LA OPERACION DE LA TIENDA 
+            	
+                indice -= 1;//CONVERTIMOS EL NÚMERO A INDICE DE LISTA
                 
                 if (indice>=productos.size() || indice<0) { //COMPROBAMOS SI EL INDICE ES CORRECTO 
-                	return "No pudimos realizar la compra, el índice es incorrecto.";
+                	return "No pudimos realizar la compra, el índice no es válido.";
                 }
                 
-                int cantidad = productos.get(indice).getCantidadUnidades();
+                int cantidad = productos.get(indice).getCantidadUnidades(); // SE TOMA LA CANTIDAD DEL PRODUCTO
 
                 if (cantidad!=0){// SI LA CANTIDAD ES DIFERENTE DE 0 SE HACE LA COMPRA
                     cantidad-=1;
-                    productos.get(indice).setCantidadUnidades(cantidad);
+                    productos.get(indice).setCantidadUnidades(cantidad); // SE TRAE EL PRODUCTO DE LA LISTA DE PRODUCTOS 
                     String nombre = productos.get(indice).getNombre();
                     String tipo =  productos.get(indice).getTipoAnimal();
+                
                     if (productos.get(indice).getCantidadUnidades()==0) {
-                    	productos.remove(indice);
+                    	productos.remove(indice); // SI LAS UNIDADES QUEDAN EN CERO, SE ELIMINA EL PRODUCTO DE LA LISTA 
                     }
                     
                     cliente = CentroAdopcion.isCliente(cliente); // SE COMPRUEBA SI EL CLIENTE EXISTE 
-                    float precio = productos.get(indice).getPrecio();
-                    int puntos = cliente.getPuntos(); //SE TOMAN LOS PUNTOS 
+                    float precio = productos.get(indice).getPrecio(); // SE GUARDA EL PRECIO PARA LA FUTURA FACTURA 
+                    int puntos = cliente.getPuntos(); //SE TOMAN LOS PUNTOS DEL CLIENTE
                     
-                    if (puntos>=15) {
-                    	cliente.disminuir_puntos(puntos);
-                    	precio -= precio*0.1;
+                    if (puntos>=15) {// SI TIENE MÁS DE 15 PUNTOS ACOMULADOS, SE LE HACE UN DESCUENTO AUTOMÁTICAMENTE
+                    	cliente.disminuir_puntos(puntos); // SE LE QUITAN LOS 15 PUNTOS 
+                    	precio -= precio*0.1; // SE MODIFICA EL PRECIO ORIGINAL 
+                    	// EN EL RETURN DEVOLVEMOS LO IMPORTADO 
                     	 return "-------------------------------------------\n"+"Has comprado una unidad de: "+nombre+" para "+tipo+"\n"+"Total a pagar: "+precio+" $"+"\n"+"Han sido utilizados 15 puntos para un 10% de descuento\n"+"Puntos restantes: "+cliente.getPuntos()+"\n-------------------------------------------";
                     }else {
                     	return "-------------------------------------------\n"+"Has comprado una unidad de: "+nombre+" para "+tipo+"\n"+"Total a pagar: "+precio+" $"+"\n"+"No cuentas con los suficientes puntos como para un descuento\n"+"-------------------------------------------";
@@ -187,37 +192,38 @@ public class Tienda implements Serializable{
             }
         }
         
-        //--------------------------------------------------------------------------------------------------------------------
         
         public String compra(int indice, int unidades, Cliente cliente){
-            if (empleados!=null){
+        	
+            if (empleados!=null){ // SE COMPRUEBA SI HAY UN EMPLEADO PARA LA OPERACION DE LA TIENDA  
+            	
                 indice -= 1; //CONVERTIMOS EL NÚMERO A INDICE
                 
                 if (indice>=productos.size() || indice<0) { //COMPROBAMOS SI EL INDICE ES CORRECTO 
                 	return "No pudimos realizar la compra, el índice es incorrecto.";
                 }
-                int cantidad = productos.get(indice).getCantidadUnidades();
+                int cantidad = productos.get(indice).getCantidadUnidades(); //GUARDAMOS LA CANTIDAD DE UNIDADES DEL PRODUCTO A COMPRAR
 
-                if (cantidad!=0 && cantidad>=unidades){//SI LA CANTIDAD INGRESADA ESTÁ DENTRO DEL RANGO
+                if (cantidad!=0 && cantidad>=unidades){//SI LA CANTIDAD INGRESADA ESTÁ DENTRO DEL RANGO INICIA LA OPERACION DE LA TIENDA 
                 	
-                    cantidad-=unidades;
-                    productos.get(indice).setCantidadUnidades(cantidad);
+                    cantidad-=unidades; // SE RESTA LAS UNIDADES QUE SE COMPRARON 
+                    productos.get(indice).setCantidadUnidades(cantidad); // SE CAMBIA LAS UNIDADES RESTANTES EN EL PRODUCTO
                     String nombre = productos.get(indice).getNombre();
                     String tipo = productos.get(indice).getTipoAnimal(); 
                     if (productos.get(indice).getCantidadUnidades()==0) {
-                    	productos.remove(indice);
+                    	productos.remove(indice);// SI LA CANTIDAD LLEGA A 0, SE ELIMINA EL PRODUCTO  
                     	
                     }
                     
                     cliente = CentroAdopcion.isCliente(cliente); // SE COMPRUEBA SI EL CLIENTE EXISTE 
-                    float precio = productos.get(indice).getPrecio();
+                    float precio = productos.get(indice).getPrecio(); // SE GUARDA EL PRECIO PARA GENERAR DESPUES FACTURA 
                     int puntos = cliente.getPuntos(); //SE TOMAN LOS PUNTOS 
                     
-                    if (puntos>=15) {
-                    	cliente.disminuir_puntos(puntos);
-                    	precio -= precio*unidades*0.1;
+                    if (puntos>=15) { // SI TIENE MÁS DE 15 PUNTOS ACOMULADOS, SE LE HACE UN DESCUENTO AUTOMÁTICAMENTE
+                    	cliente.disminuir_puntos(puntos); // SE RESTAN LOS 15 PUNTOS QUE SE USARON COMO DESCUENTO
+                    	precio -= precio*unidades*0.1; // SE CALCULA EL PRECIO CON EL 10% DE DESCUENTO
                     	 return "-------------------------------------------\n"+"Has comprado "+unidades+" unidades de: "+nombre+" para "+tipo+"\n"+"Total a pagar: "+precio+" $"+"\n"+"Han sido utilizados 15 puntos para un 10% de descuento\n"+"Puntos restantes: "+cliente.getPuntos()+"\n-------------------------------------------";
-                    }else {
+                    }else {// DEVUELVE SOLAMENTE PRECIO PORQUE NO TIENE LOS PUNTOS SUFUCIENTES
                     	return "-------------------------------------------\n"+"Has comprado "+unidades+" unidades de: "+nombre+" para "+tipo+"\n"+"Total a pagar: "+precio*unidades+" $"+"\n"+"No cuentas con los suficientes puntos como para un descuento\n"+"-------------------------------------------";
                     }
                 }
